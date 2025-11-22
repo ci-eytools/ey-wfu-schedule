3665bf7b564f65d8f92d9b64b720ef5fdef83066
ci-eytools <ci-eytools@outlook.com>
2025-11-23 00:18:15 +0800

    refactor(datastore): 重构DataStore模块结构并迁移实现

--- app/src/main/java/com/atri/seduley/data/local/datastore/di/DatastoreModule.kt
+++ app/src/main/java/com/atri/seduley/data/local/datastore/di/DatastoreModule.kt
@@ -1,31 +1,61 @@
 package com.atri.seduley.data.local.datastore.di
 
+import android.content.Context
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
 import com.atri.seduley.data.local.datastore.CredentialDatastore
 import com.atri.seduley.data.local.datastore.SystemDatastore
 import com.atri.seduley.data.local.datastore.security.CryptoManager
-import com.atri.seduley.di.DataStoreModule
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Qualifier
 import javax.inject.Singleton
+
+private val Context.credentialDataStore: DataStore<Preferences> by preferencesDataStore(name = "credential_prefs")
+private val Context.systemDataStore: DataStore<Preferences> by preferencesDataStore(name = "system_prefs")
 
 @Module
 @InstallIn(SingletonComponent::class)
-class DatastoreModule {
+object DatastoreModule {
+
+    @Qualifier
+    @Retention(AnnotationRetention.BINARY)
+    annotation class Credential
+
+    @Qualifier
+    @Retention(AnnotationRetention.BINARY)
+    annotation class System
+
+    /**
+     * 凭证相关
+     */
+    @Provides
+    @Singleton
+    @Credential
+    fun provideCredentialDataStore(@ApplicationContext context: Context) = context.credentialDataStore
+
+    /**
+     * 系统配置相关
+     */
+    @Provides
+    @Singleton
+    @System
+    fun provideSystemDataStore(@ApplicationContext context: Context) = context.systemDataStore
 
     @Provides
     @Singleton
     fun provideCredentialDatastore(
-        @DataStoreModule.Credential dataStore: DataStore<Preferences>,
+        @Credential dataStore: DataStore<Preferences>,
         cryptoManager: CryptoManager
     ) = CredentialDatastore(dataStore, cryptoManager)
 
     @Provides
     @Singleton
     fun provideSystemDatastore(
-        @DataStoreModule.System dataStore: DataStore<Preferences>
+        @System dataStore: DataStore<Preferences>
     ) = SystemDatastore(dataStore)
 }

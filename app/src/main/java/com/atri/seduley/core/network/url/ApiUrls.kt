f97bb187728b620f66f6e2b97680ec456f8d01e9
ci-eytools <ci-eytools@outlook.com>
2025-11-23 15:43:49 +0800

    docs(network): 添加 API 接口注释说明请求方法类型

--- app/src/main/java/com/atri/seduley/core/network/url/ApiUrls.kt
+++ app/src/main/java/com/atri/seduley/core/network/url/ApiUrls.kt
@@ -1,10 +1,20 @@
 package com.atri.seduley.core.network.url
 
 enum class ApiUrls(val path: String) {
+
+    /** POST */
     LOGIN("Logon.do?method=logon"),
+
+    /** POST */
     CAPTCHA("verifycode.servlet"),
+
+    /** GET */
     SESS("Logon.do?method=logon&flag=sess"),
-    STUDENT_MAIN_PAGE("jsxsd/framework/xsMain.jsp"),
+
+    /**
+     * POST
+     * params={'rq': '2025-09-12'}
+     */
     COURSE_PAGE("jsxsd/framework/main_index_loadkb.jsp");
 
     companion object {

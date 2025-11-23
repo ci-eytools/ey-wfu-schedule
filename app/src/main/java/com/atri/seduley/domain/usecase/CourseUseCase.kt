a9474ecebc9278a2e209a6ab811cf964f340d3d1
ci-eytools <ci-eytools@outlook.com>
2025-11-23 22:44:14 +0800

    refactor(repository): 统一异常处理并优化课表获取逻辑

--- app/src/main/java/com/atri/seduley/domain/usecase/CourseUseCase.kt
+++ app/src/main/java/com/atri/seduley/domain/usecase/CourseUseCase.kt
@@ -2,7 +2,6 @@ package com.atri.seduley.domain.usecase
 
 import com.atri.seduley.core.exception.CredentialException
 import com.atri.seduley.core.exception.NetworkException
-import com.atri.seduley.domain.model.Course
 import com.atri.seduley.domain.repository.AuthRepository
 import com.atri.seduley.domain.repository.CourseRepository
 import com.atri.seduley.domain.result.CourseResult
@@ -17,14 +16,17 @@ data class CourseUseCase @Inject constructor(
     /** 获取课表 */
     suspend fun getCourses(
         studentId: String? = null
-    ): List<Course> {
-        val currStudentId = parseStudentId(studentId)
-        val coursesDB = courseRepository.getCoursesFromDB(currStudentId)
-        if (coursesDB.isNotEmpty()) return coursesDB
-
-        val coursesRemote = courseRepository.getCoursesFromRemote(currStudentId)
-        courseRepository.updateCourse(currStudentId, coursesRemote)
-        return coursesRemote
+    ): CourseResult {
+        return try {
+            val currStudentId = parseStudentId(studentId)
+            val coursesDB = courseRepository.getCoursesFromDB(currStudentId)
+            if (coursesDB.isNotEmpty()) CourseResult.Success(coursesDB)
+            val coursesRemote = courseRepository.getCoursesFromRemote(currStudentId)
+            courseRepository.updateCourse(currStudentId, coursesRemote)
+            CourseResult.Success(coursesRemote)
+        } catch (e: Exception) {
+            handlerException(e)
+        }
     }
 
     /** 更新课表 */
@@ -37,12 +39,8 @@ data class CourseUseCase @Inject constructor(
             val courses = courseRepository.getCoursesFromRemote(currStudentId)
             courseRepository.updateCourse(currStudentId, courses)
             CourseResult.Success(courses)
-        } catch (e: CredentialException) {
-            return CourseResult.AuthError(e.message)
-        } catch (e: NetworkException) {
-            return CourseResult.AuthError(e.message)
-        } catch (_: Exception) {
-            return CourseResult.UnknownError
+        } catch (e: Exception) {
+            handlerException(e)
         }
     }
 
@@ -54,19 +52,33 @@ data class CourseUseCase @Inject constructor(
             val currStudentId = parseStudentId(studentId)
             courseRepository.clearCourses(currStudentId)
             CourseResult.Success(emptyList())
-        } catch (e: CredentialException) {
-            return CourseResult.AuthError(e.message)
-        } catch (_: Exception) {
-            return CourseResult.UnknownError
+        } catch (e: Exception) {
+            handlerException(e)
         }
     }
 
+    /** 解析用户 id */
     private suspend fun parseStudentId(studentId: String?): String {
         val allIds = authRepository.getAllStudentId().first()
         return when {
             studentId != null && allIds.contains(studentId) -> studentId
             studentId == null && allIds.isNotEmpty() -> allIds.first()
             else -> throw CredentialException("未登录或未找到用户")
+        }
+    }
+
+    /** 异常处理 */
+    private fun handlerException(e: Exception): CourseResult {
+        return when(e) {
+            is CredentialException -> {
+                CourseResult.AuthError(e.message)
+            }
+            is NetworkException -> {
+                CourseResult.AuthError(e.message)
+            }
+            else -> {
+                CourseResult.UnknownError
+            }
         }
     }
 }

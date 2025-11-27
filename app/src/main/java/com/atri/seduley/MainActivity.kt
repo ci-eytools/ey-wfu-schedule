56b100990d9678939b4141f2b66a2902cc895e11
ci-eytools <ci-eytools@outlook.com>
2025-11-28 00:45:04 +0800

    fix(activity): 修复 MainActivity 依赖注入和状态收集问题

--- app/src/main/java/com/atri/seduley/MainActivity.kt
+++ app/src/main/java/com/atri/seduley/MainActivity.kt
@@ -5,20 +5,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.viewModels
 import androidx.annotation.RequiresApi
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.core.view.WindowCompat
+import com.atri.seduley.data.local.sp.SystemProvider
 import com.atri.seduley.ui.navigation.AppNav
 import com.atri.seduley.ui.theme.AppTheme
-import com.atri.seduley.ui.viewmodel.SettingViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
-    private val settingViewModel: SettingViewModel by viewModels()
+    @Inject
+    lateinit var systemProvider: SystemProvider
 
     @RequiresApi(Build.VERSION_CODES.S)
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,7 +27,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         WindowCompat.setDecorFitsSystemWindows(window, false)
         setContent {
-            val seedColor by settingViewModel.seedColor.collectAsState()
+            val seedColor by systemProvider.seedColorFlow.collectAsState()
             AppTheme(seedColor) {
                 AppNav()
             }

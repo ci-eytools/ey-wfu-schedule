9eeb4e3b081e0c0f2042a33969524ad327bb784d
ci-eytools <ci-eytools@outlook.com>
2025-11-29 15:03:37 +0800

    refactor(domain): 重构课程用例逻辑并更新依赖注入

--- app/src/main/java/com/atri/seduley/domain/usecase/CourseUseCase.kt
+++ app/src/main/java/com/atri/seduley/domain/usecase/CourseUseCase.kt
@@ -1,11 +1,14 @@
 package com.atri.seduley.domain.usecase
 
 import com.atri.seduley.core.exception.CredentialException
-import com.atri.seduley.core.exception.NetworkException
-import com.atri.seduley.core.util.AppLogger
+import com.atri.seduley.domain.model.Course
 import com.atri.seduley.domain.repository.AuthRepository
 import com.atri.seduley.domain.repository.CourseRepository
-import com.atri.seduley.domain.result.CourseResult
+import com.atri.seduley.domain.result.Result
+import com.atri.seduley.domain.result.toReturn
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.first
+import java.time.LocalDate
 import javax.inject.Inject
 
 data class CourseUseCase @Inject constructor(
@@ -13,77 +16,45 @@ data class CourseUseCase @Inject constructor(
     private val authRepository: AuthRepository
 ) {
 
-    /** 获取课表 */
-    suspend fun getCourses(
-        studentId: String? = null
-    ): CourseResult {
-        return try {
-            val currStudentId = resolveStudentId(studentId)
-            val coursesDB = courseRepository.getCoursesFromDB(currStudentId)
-            if (coursesDB.isNotEmpty()) {
-                return CourseResult.Success(coursesDB)
-            }
-            authRepository.loginAs(currStudentId)
-            val coursesRemote = courseRepository.getCoursesFromRemote(currStudentId)
-            courseRepository.updateCourse(currStudentId, coursesRemote)
-            CourseResult.Success(coursesRemote)
-        } catch (e: Exception) {
-            handlerException(e)
-        }
+    /**
+     * 观察课表
+     *
+     * 向数据库请求
+     */
+    suspend fun observeCourses(
+        studentId: String? = null,
+        date: LocalDate = LocalDate.now()
+    ): Result<Flow<List<Course?>>> = toReturn {
+        val currStudentId = resolveStudentId(studentId?.toLong())
+        courseRepository.observeCoursesByStudentIdAndDate(currStudentId, date)
     }
 
     /** 更新课表 */
     suspend fun updateCourseFromRemote(
         studentId: String? = null,
         isNeedLogin: Boolean = true
-    ): CourseResult {
-        return try {
-            val currStudentId = resolveStudentId(studentId)
-            if (isNeedLogin) authRepository.loginAs(currStudentId)
-            val courses = courseRepository.getCoursesFromRemote(currStudentId)
-            courseRepository.updateCourse(currStudentId, courses)
-            CourseResult.Success(courses)
-        } catch (e: Exception) {
-            handlerException(e)
-        }
+    ): Result<Unit> = toReturn {
+        val currStudentId = resolveStudentId(studentId?.toLong())
+        if (isNeedLogin) authRepository.loginAs(currStudentId)
+        val courses = courseRepository.getAllCoursesFromRemote(currStudentId)
+        courseRepository.insertCourses(currStudentId, courses)
     }
 
     /** 清除课表 */
     suspend fun clearCourse(
         studentId: String? = null
-    ): CourseResult {
-        return try {
-            val currStudentId = resolveStudentId(studentId)
-            courseRepository.clearCourses(currStudentId)
-            CourseResult.Success(emptyList())
-        } catch (e: Exception) {
-            handlerException(e)
-        }
+    ): Result<Unit> = toReturn {
+        val currStudentId = resolveStudentId(studentId?.toLong())
+        courseRepository.clearCourses(currStudentId)
     }
 
     /** 解析用户 id */
-    private suspend fun resolveStudentId(studentId: String?): String {
-        val allIds = authRepository.getAllStudentId()
+    private suspend fun resolveStudentId(studentId: Long?): Long {
+        val allIds = authRepository.observeStudentIds().first()
         return when {
             studentId != null && allIds.contains(studentId) -> studentId
             studentId == null && allIds.isNotEmpty() -> allIds.first()
             else -> throw CredentialException("未登录或未找到用户")
-        }
-    }
-
-    /** 异常处理 */
-    private fun handlerException(e: Exception): CourseResult {
-        return when(e) {
-            is CredentialException -> {
-                CourseResult.AuthError(e.message)
-            }
-            is NetworkException -> {
-                CourseResult.AuthError(e.message)
-            }
-            else -> {
-                AppLogger.e(e)
-                CourseResult.UnknownError
-            }
         }
     }
 }

17d6c9bb105a3051fce9b40d3cb14fb600efaebd
ci-eytools <ci-eytools@outlook.com>
2025-11-30 14:06:33 +0800

    fix(database): 使用系统默认时区进行本地日期时间转换

--- app/src/main/java/com/atri/seduley/data/local/database/converter/Converters.kt
+++ app/src/main/java/com/atri/seduley/data/local/database/converter/Converters.kt
@@ -10,11 +10,13 @@ class Converters {
 
     @TypeConverter
     fun fromLocalDateTime(value: LocalDateTime?): Long? =
-        value?.atZone(ZoneId.of("UTC"))?.toInstant()?.toEpochMilli()
+        value?.atZone(ZoneId.systemDefault())?.toInstant()?.toEpochMilli()
 
     @TypeConverter
     fun toLocalDateTime(value: Long?): LocalDateTime? =
-        value?.let { Instant.ofEpochMilli(it).atZone(ZoneId.of("UTC")).toLocalDateTime() }
+        value?.let {
+            Instant.ofEpochMilli(it).atZone(ZoneId.systemDefault()).toLocalDateTime()
+        }
 
     @TypeConverter
     fun fromLocalDate(value: LocalDate?): Long? =

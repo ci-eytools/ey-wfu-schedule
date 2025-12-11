2624ce34a6db6168ee0aac9a8b7b197e3515543c
ci-eytools <ci-eytools@outlook.com>
2025-12-11 23:15:53 +0800

    feat: 优化 Palette 的 defaultColor 为 中性灰

--- app/src/main/java/com/atri/seduley/ui/theme/ColorExtraction.kt
+++ app/src/main/java/com/atri/seduley/ui/theme/ColorExtraction.kt
@@ -6,13 +6,15 @@ import androidx.palette.graphics.Palette
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
+private const val FALLBACK_COLOR = 0xFF777777.toInt()
+
 suspend fun extractDominantColor(
     bitmap: Bitmap,
-    defaultColor: Int
+    defaultColor: Int = FALLBACK_COLOR
 ): Color = withContext(Dispatchers.Default) {
 
     val palette = Palette.from(bitmap).generate()
-    val intColor = palette.getDominantColor(defaultColor)
 
+    val intColor = palette.getDominantColor(defaultColor)
     Color(intColor)
 }
